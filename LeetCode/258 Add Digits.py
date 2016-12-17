825eb88cc45ced265934e203a97c711ef77967d3
LuffySir <luffysir@mail.dlut.edu.cn>
2016-12-17 20:45:13 +0800

    258
    左子树的值的和

--- LeetCode/258 Add Digits.py
+++ LeetCode/258 Add Digits.py
@@ -1,19 +1,39 @@
-#  Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-
-# For example:
-
-# Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
-
-# Follow up:
-# Could you do it without any loop/recursion in O(1) runtime?
+#  Find the sum of all left leaves in a given binary tree.
 
 
-def addDigits(self, num):
-    """
-    :type num: int
-    :rtype: int
-    """
-    if num < 9:
-        return num
-    else:
-        return 9 if num % 9 == 0 else num % 9
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sumOfLeftLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        sum = 0
+        if not root:
+            return 0
+        if root.left and not root.left.left and not root.left.right:
+            sum += root.left.val
+        sum += self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+        return sum
+
+    def sumOfLeftLeaves(self, root):
+        if not root:
+            return 0
+
+        s = [root]
+        res = 0
+        while s:
+            tmp = s.pop()
+            if tmp.left:
+                s.append(tmp.left)
+                if not tmp.left.left and not tmp.left.right:
+                    res += tmp.left.val
+            if tmp.right:
+                s.append(tmp.right)
+        return res
