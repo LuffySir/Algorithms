6f425143c133eb4d9548bcc62e26dc89b83eb03b
LuffySir <luffysir@mail.dlut.edu.cn>
2016-12-19 16:37:50 +0800

    100
    判断两个二叉树是否相等

---
+++ LeetCode/100 Same Tree.py
@@ -0,0 +1,57 @@
+# Given a string, find the first non-repeating character in it and
+# return it's index. If it doesn't exist, return -1.
+
+#  Given two binary trees, write a function to check if they are equal or not.
+
+# Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p == q
+
+    # DFS with stack:
+    def isSameTree2(self, p, q):
+        stack = [(p, q)]
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                stack.append((node1.right, node2.right))
+                stack.append((node1.left, node2.left))
+        return True
+
+    # BFS with queue:
+    def isSameTree3(self, p, q):
+        queue = [(p, q)]
+        while queue:
+            node1, node2 = queue.pop()
+            if not node1 and not node2:
+                continue
+            elif None in [node1, node2]:
+                return False
+            else:
+                if node1.val != node2.val:
+                    return False
+                queue.append((node1.left, node2.left))
+                queue.append((node1.right, node2.right))
+        return True
