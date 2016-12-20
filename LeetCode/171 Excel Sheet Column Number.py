6fab833f92ce90f8e31c607d295d54e1921b3180
LuffySir <luffysir@mail.dlut.edu.cn>
2016-12-20 16:45:36 +0800

    171
    给excel列编号

---
+++ LeetCode/171 Excel Sheet Column Number.py
@@ -0,0 +1,20 @@
+# Given a column title as appear in an Excel sheet, return its corresponding column number.
+# A -> 1
+# B -> 2
+# C -> 3
+# ...
+# Z -> 26
+# AA -> 27
+# AB -> 28
+
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num = 0
+        for i in range(len(s) - 1, -1, -1):
+            num += (ord(s[len(s) - 1 - i]) - ord('A') + 1) * 26 ** i
+        return num
