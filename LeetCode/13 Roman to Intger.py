1c8c5c6745a7ad6b266f632efc74fd48fb989e49
LuffySir <luffysir@mail.dlut.edu.cn>
2016-12-26 14:46:57 +0800

    13
    罗马数字转换成整数

---
+++ LeetCode/13 Roman to Intger.py
@@ -0,0 +1,27 @@
+#  Given a roman numeral, convert it to an integer.
+
+# Input is guaranteed to be within the range from 1 to 3999.
+
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        d = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+        res, p = 0, 'I'
+        for c in s[::-1]:
+            res, p = res - d[c] if d[c] < d[p] else res + d[c], c
+        return res
+
+    def romanToInt1(self, s):
+        roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+        z = 0
+        for i in range(0, len(s) - 1):
+            if roman[s[i]] < roman[s[i + 1]]:
+                z -= roman[s[i]]
+            else:
+                z += roman[s[i]]
+        return z + roman[s[-1]]
