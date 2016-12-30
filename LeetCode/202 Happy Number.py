af2f20da66833f29c56084c889a53206f8406440
LuffySir <luffysir@mail.dlut.edu.cn>
2016-12-30 22:01:35 +0800

    202
    找到happynumber

---
+++ LeetCode/202 Happy Number.py
@@ -0,0 +1,35 @@
+# A happy number is a number defined by the following process: Starting with any positive integer,
+# replace the number by the sum of the squares of its digits, and repeat the process until the number
+# equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those
+# numbers for which this process ends in 1 are happy numbers.
+
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        s = set()
+        while n != 1:
+            a_sum = 0
+            n_str = str(n)
+            for i in range(len(n_str)):
+                a_sum += int(n_str[i]) * int(n_str[i])
+            if a_sum in s:
+                return False
+            else:
+                s.add(a_sum)
+            n = a_sum
+        return True
+
+    def isHappy1(self, n):
+        mem = set()
+        while n != 1:
+            n = sum([int(i) ** 2 for i in str(n)])
+            if n in mem:
+                return False
+            else:
+                mem.add(n)
+        else:
+            return True
