e325270607e8fb88a3a14873be28a3c6146abf5d
LuffySir <luffysir@mail.dlut.edu.cn>
2017-01-12 16:23:50 +0800

    437
    找到二叉树中和为给定值的所有路径

---
+++ LeetCode/437 Path Sum 3.py
@@ -0,0 +1,47 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if root:
+            return self.find_paths(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return 0
+
+    def find_paths(self, root, target):
+        if root:
+            return int(root.val == target) + self.find_paths(root.left, target - root.val) + self.find_paths(root.right, target - root.val)
+        return 0
+
+
+class Solution2(object):
+    def helper(self, root, target, so_far, cache):
+        if root:
+            complement = so_far + root.val - target
+            if complement in cache:
+                self.result += cache[complement]
+            cache.setdefault(so_far + root.val, 0)
+            cache[so_far + root.val] += 1
+            self.helper(root.left, target, so_far + root.val, cache)
+            self.helper(root.right, target, so_far + root.val, cache)
+            cache[so_far + root.val] -= 1
+        return
+
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        self.result = 0
+        self.helper(root, sum, 0, {0: 1})
+        return self.result
