7ad8a8d32b7bae5969dd4cf1ed1bb8905b6670a8
LuffySirLeopold <370310075@qq.com>
2017-02-06 21:25:01 +0800

    257
    返回二叉树所有路径

--- LeetCode/141 Linked List Cycle.py
+++ LeetCode/141 Linked List Cycle.py
@@ -8,22 +8,8 @@
 #         self.next = None
 
 class Solution(object):
-    # 超时
-    def hasCycle(self, head):
-        """
-        :type head: ListNode
-        :rtype: bool
-        """
-        if not head:
-            return False
-        curr, sofar = head, head
-        while sofar:
-            if sofar.next == curr:
-                return True
-            sofar = sofar.next
-        return self.hasCycle(head.next)
 
-    def hasCycle1(self, head):
+    def hasCycle(self, head):
         slow = fast = head
         while fast and fast.next:
             fast = fast.next.next
