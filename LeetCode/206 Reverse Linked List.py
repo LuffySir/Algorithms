efebd2b2f2a9d310b9268bd52977dcd7d995386f
LuffySir <luffysir@mail.dlut.edu.cn>
2017-03-16 15:54:04 +0800

    206
    翻转链表

--- LeetCode/206 Reverse Linked List.py
+++ LeetCode/206 Reverse Linked List.py
@@ -8,8 +8,23 @@
 #         self.next = None
 
 class Solution(object):
+    # 非递归
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+    def reverseList2(self, head, last = None):
+        if not head:
+            return head
+        next = head.next
+        head.next = last
+        return self.reverseList2(next, head)
